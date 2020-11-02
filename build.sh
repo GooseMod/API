dbf8c8ceec898a046242551f03bcadd3510f6dbf
Oj <oj@oojmed.com>
2020-11-02 21:32:34 +0000

    Add version JSON and txt files for inject.js

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ rm -rf out
 mkdir out
 
 mkdir out/untethered
+mkdir out/injectVersion
 
 # NPM install for Node script
 cd modulesJson
@@ -29,6 +30,11 @@ cp _headers out/_headers
 
 # Copy latest inject.js to out (for untethered)
 wget https://raw.githubusercontent.com/GooseMod/Injector/master/dist/index.js -O out/inject.js
+
+cat out/inject.js | grep "this.versionHash=\".*?\"" -P -o | cut -d "\"" -f 2 > out/injectVersion/hash.txt
+cat out/inject.js | grep "this.version=\".*?\"" -P -o | cut -d "\"" -f 2 > out/injectVersion/version.txt
+
+echo "{\"version\": \"$(cat out/injectVersion/version.txt)\", \"hash\": \"$(cat out/injectVersion/hash.txt)\"}" > out/injectVersion.json
 
 # Copy latest changelog release into out generated via Node script
 cd changelog
