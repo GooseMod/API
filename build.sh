fb5493292317f9588ee1386d2bab8780eaaa5f57
Oj <oj@oojmed.com>
2021-02-28 21:24:21 +0000

    [Chore] Update Injector repo URLs to new GitHub repo name

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ cd ..
 cp _headers out/_headers
 
 # Copy latest inject.js to out (for untethered)
-wget https://raw.githubusercontent.com/GooseMod/Injector/master/dist/index.js -O out/inject.js
+wget https://raw.githubusercontent.com/GooseMod/GooseMod/master/dist/index.js -O out/inject.js
 
 cat out/inject.js | grep "this.versionHash=\".*?\"" -P -o | cut -d "\"" -f 2 > out/injectVersion/hash.txt
 cat out/inject.js | grep "this.version=\".*?\"" -P -o | cut -d "\"" -f 2 > out/injectVersion/version.txt
@@ -38,7 +38,7 @@ echo "{\"version\": \"$(cat out/injectVersion/version.txt)\", \"hash\": \"$(cat 
 
 # Copy latest changelog release into out generated via Node script
 cd changelog
-wget https://raw.githubusercontent.com/GooseMod/Injector/master/CHANGELOG.md -O changelog.md
+wget https://raw.githubusercontent.com/GooseMod/GooseMod/master/CHANGELOG.md -O changelog.md
 node generate.mjs
 
 cd ..
