5e95c372d14f67834689f8ea7e4ad5edbf0f3ccf
Oj <oj@oojmed.com>
2021-02-28 21:34:42 +0000

    [build.sh] Add no cache flag to wget commands

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ cd ..
 cp _headers out/_headers
 
 # Copy latest inject.js to out (for untethered)
-wget https://raw.githubusercontent.com/GooseMod/GooseMod/master/dist/index.js -O out/inject.js
+wget --no-cache https://raw.githubusercontent.com/GooseMod/GooseMod/master/dist/index.js -O out/inject.js
 
 cat out/inject.js | grep "this.versionHash=\".*?\"" -P -o | cut -d "\"" -f 2 > out/injectVersion/hash.txt
 cat out/inject.js | grep "this.version=\".*?\"" -P -o | cut -d "\"" -f 2 > out/injectVersion/version.txt
@@ -38,11 +38,11 @@ echo "{\"version\": \"$(cat out/injectVersion/version.txt)\", \"hash\": \"$(cat 
 
 # Copy latest changelog release into out generated via Node script
 cd changelog
-wget https://raw.githubusercontent.com/GooseMod/GooseMod/master/CHANGELOG.md -O changelog.md
+wget --no-cache https://raw.githubusercontent.com/GooseMod/GooseMod/master/CHANGELOG.md -O changelog.md
 node generate.mjs
 
 cd ..
 
 # Copy latest untethered JS stuff to out (for untethered)
-wget https://raw.githubusercontent.com/GooseMod/Untethered/master/src/untetheredInject.js -O out/untethered/untetheredInject.js
+wget --no-cache https://raw.githubusercontent.com/GooseMod/Untethered/master/src/untetheredInject.js -O out/untethered/untetheredInject.js
 # wget https://raw.githubusercontent.com/GooseMod/Untethered/master/src/base.js -O out/untethered/base.js
