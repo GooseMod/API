a00fcdf154c2b72f94a2aec822d9996691e9dfb5
Oj <oj@oojmed.com>
2021-07-29 21:32:38 +0100

    Comment out old unused stuff, selfhost PGP library

--- build.sh
+++ build.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 # Clean up and make build + out dir
-rm -rf build
-mkdir build
+# rm -rf build
+# mkdir build
 
 rm -rf out
 mkdir out
 
-mkdir out/untethered
+# mkdir out/untethered
 mkdir out/injectVersion
 
 # NPM install for Node script
-cd modulesJson
-npm install
+# cd modulesJson
+# npm install
 
 
-cd ../build
+# cd ../build
 
 # Clone repo
-git clone https://github.com/GooseMod/Modules clone
+# git clone https://github.com/GooseMod/Modules clone
 
 # Use Node script to generate JSON and copy over minified JS files
-node ../modulesJson/generate.mjs
+# node ../modulesJson/generate.mjs
 
 # Copy _headers
-cd ..
+# cd ..
 
 cp _headers out/_headers
 
@@ -36,13 +36,16 @@ cat out/inject.js | grep "this.version=\".*?\"" -P -o | cut -d "\"" -f 2 > out/i
 
 echo "{\"version\": \"$(cat out/injectVersion/version.txt)\", \"hash\": \"$(cat out/injectVersion/hash.txt)\"}" > out/injectVersion.json
 
-# Copy latest changelog release into out generated via Node script
-cd changelog
-wget --no-cache https://raw.githubusercontent.com/GooseMod/GooseMod/master/CHANGELOG.md -O changelog.md
-node generate.mjs
+# Copy PGP library
+wget "https://unpkg.com/openpgp@5.0.0-5/dist/openpgp.min.js" -O out/pgp.js
 
-cd ..
+# Copy latest changelog release into out generated via Node script
+# cd changelog
+# wget --no-cache https://raw.githubusercontent.com/GooseMod/GooseMod/master/CHANGELOG.md -O changelog.md
+# node generate.mjs
+
+# cd ..
 
 # Copy latest untethered JS stuff to out (for untethered)
-wget --no-cache https://raw.githubusercontent.com/GooseMod/Untethered/master/src/untetheredInject.js -O out/untethered/untetheredInject.js
+# wget --no-cache https://raw.githubusercontent.com/GooseMod/Untethered/master/src/untetheredInject.js -O out/untethered/untetheredInject.js
 # wget https://raw.githubusercontent.com/GooseMod/Untethered/master/src/base.js -O out/untethered/base.js
